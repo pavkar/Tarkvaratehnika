d591e321d5230242a6c9a53396fe939219905fcb
kasesalum <mekase@ttu.ee>
2018-03-06 10:32:03 +0200

    frontend 1. iteratsiooni kaitsmiseks
    Merlini poolt edititud, @pavkar võta see üle?

---
+++ frontend src/main.js
@@ -0,0 +1,26 @@
+import environment from './environment';
+
+export function configure(aurelia) {
+  aurelia.use
+    .standardConfiguration()
+    .basicConfiguration()
+    .feature('resources')
+    .defaultBindingLanguage()
+    .defaultResources()
+    .developmentLogging()
+    .router()
+    .history()
+    .eventAggregator()
+
+  if (environment.debug) {
+    aurelia.use.developmentLogging();
+  }
+
+  if (environment.testing) {
+    aurelia.use.plugin('aurelia-testing');
+  }
+
+  aurelia.start().then(() => aurelia.setRoot());
+
+  
+}
