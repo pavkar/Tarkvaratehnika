674bb8a4b9a2fdef13dc8bca8a0f950a0df6bbd6
kasesalum <mekase@ttu.ee>
2018-03-13 12:55:39 +0200

    #5 retseptide parameetrite küsimiseks endpointid

--- demo/src/main/java/com/example/demo/Recipe/RecipeController.java
+++ demo/src/main/java/com/example/demo/Recipe/RecipeController.java
@@ -1,6 +1,7 @@
 package com.example.demo.Recipe;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,5 +20,30 @@ public class RecipeController {
 	public Recipe addRecipe(@RequestBody Recipe recipe) {
 		return recipeService.addRecipe(recipe);
 		
+	}
+	
+	@RequestMapping(value = "/recipes/ingredients/{id}", method=RequestMethod.GET)
+	public String getIngredients(@PathVariable("id") long id) {
+		return recipeService.getRecipeIngredients(id);
+	}
+	
+	@RequestMapping(value = "/recipes/instructions/{id}", method=RequestMethod.GET)
+	public String getInstructions(@PathVariable("id") long id) {
+		return recipeService.getRecipeInstructions(id);
+	}
+	
+	@RequestMapping(value = "/recipes/name/{id}", method=RequestMethod.GET)
+	public String getName(@PathVariable("id") long id) {
+		return recipeService.getRecipeName(id);
+	}
+	
+	@RequestMapping(value = "/recipes/time/{id}", method=RequestMethod.GET)
+	public String getTime(@PathVariable("id") long id) {
+		return recipeService.getRecipeTime(id);
+	}
+	
+	@RequestMapping(value = "/recipes/size/{id}", method=RequestMethod.GET)
+	public int getSize(@PathVariable("id") long id) {
+		return recipeService.getRecipeSize(id);
 	}
 }
